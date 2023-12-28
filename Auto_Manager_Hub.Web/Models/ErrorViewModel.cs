a69da1047827664cf575085add4377586f259b31
Adnan Muhaisen <adnanmohaisen609@gmail.com>
2023-12-28 09:11:29 +0300

    Add project files.

---
+++ Auto_Manager_Hub.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Auto_Manager_Hub.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
